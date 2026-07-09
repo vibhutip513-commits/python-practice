7ee6c1253b4d687fbce80829bc6ebefe5c008ae8
Vibhuti Prajapati <vibhutip513@gmail.com>
2026-07-09 15:07:11 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,30 @@
+#reverse String
+# a=input("")
+# b=a[::-1]
+# print(b)
+
+
+# #palindrom
+# a=input("Enter a String: ")
+# b=a[::-1]
+# if  a==b:
+#     print("Palindrom")
+# else:
+#     print("Not Palindrom")
+
+
+# #Count vowels in a String
+# a=input("Enter a String: ")
+# vowels=0
+# for i in a:
+#     if i in "aeiouAEIOU":
+#         vowels+=1
+# print("Number of vowels: ",vowels)
+
+
+#count of words in String
+a=input("Enter a String: ")
+words=a.split()
+print("Number of words: ", len(words))
+
+
