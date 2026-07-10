631ed9d2050010733d452b6e0e53ed84f96514f1
Vibhuti Prajapati <vibhutip513@gmail.com>
2026-07-10 12:28:03 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,61 @@
+
+## 1. Add 2 numbers
+#"""sabse pehle function banaya usme perameter pass kiye then return kar ke sum operator ka use kiya then function call kiya or arguments pass kr diye"""
+#def add(a,b):
+#         return "sum=",a+b
+# obj=add(19,23)
+# print(obj)
+
+# #2. square of a number: 
+"""_summary_ subse pehle ek function banaya sqrt name se usme ek perameter pass kiya usko return kara diya or usme **2 ka multiply kiya taki uski power k according squrt return ho jaye
+    """
+# def sqrt(a):
+#     return a**2
+
+# obj=sqrt(a=int(input("Enter a number:")))
+# print("Square of a number=",obj)
+
+
+
+
+# #3. check if a number is even or odd
+"""number 2 se divide ho kar reminder 0 dega to even nhi to odd simple code likh diya ,user input chaiye the to arrgument me comment pass kiya use input ka :user input lene ka or koi tareeka ho to batana
+    """
+# def even_odd(num):
+#      if num%2==0:
+#          return "Even"
+#      else:
+#          return "Odd"
+     
+# obj=even_odd(num=int(input("Enter a number:")))
+# print(obj)
+
+
+# #4.maximum of three numbers
+"""isme math library use kar k max functioncall kar diya"""
+# import math
+# a=[12,34,23,45]
+# def maximum():
+#     return max(a)
+# obj=maximum()
+# print("Max=",obj)
+
+
+
+#calculate factorial
+""" factorial ka simple logic likh kar samjha ki fact 1 lenge or usme  ek ek kar ke number se chhota value multiplye karte jayege to vo aage badh raha he bahi kiya"""
+# a=int(input("Enter a number:"))
+# fact=1
+# for i in range(1,a+1):
+#     fact*=i
+    
+# print(fact)
+   
+# def factorial(a):
+#   fact=1   
+#   for i in range(1,a+1):
+#         fact*=i
+#   print(fact)
+      
+# f=factorial(4)
+# # print("Factorial=",f)
