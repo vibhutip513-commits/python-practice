e61ce93d0e43262136d4ff5e6519ac4b770a9b2c
Vibhuti Prajapati <vibhutip513@gmail.com>
2026-07-10 09:17:07 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,57 @@
+#reverse String
+# a=input("")
+# b=a[::-1]
+# print(b)
+
+
+# #palindrom
+# a=input("Enter a String: ")
+# b=a[::-1]
+# if  a==b:
+#     print("Palindrom")
+# else:
+#     print("Not Palindrom")
+
+
+# #Count vowels in a String
+# a=input("Enter a String: ")
+# vowels=0
+# for i in a:
+#     if i in "aeiouAEIOU":
+#         vowels+=1
+# print("Number of vowels: ",vowels)
+
+
+# #count of words in String
+# a=input("Enter a String: ")
+# words=a.split()
+# print("Number of words: ", len(words))
+
+
+#count of small and capital latter
+# a="Hello World!"
+# small_count=0
+# capital_count=0
+# for i in a:
+#     if i.islower():
+#         small_count+=1
+#     elif i.isupper():
+#         capital_count+=1
+
+# print("Number of small latter:",small_count)
+# print("Number of capital latter:",capital_count)
+
+
+# #Remove duplicate latters from String
+# a="Programming"
+# b=""
+# for i in a:
+#     if i not in b:
+#         b+=i
+# print(b)
+
+
+
+
+
+    
