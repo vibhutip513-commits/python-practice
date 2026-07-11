6445627433253c794a986fdab0f436f102596aa7
Vibhuti Prajapati <vibhutip513@gmail.com>
2026-07-11 18:10:51 +0530

    Add files via upload
    Dictionary

---
+++ day5.py
@@ -0,0 +1,48 @@
+#Dictionary
+#Create a Student dictionary
+student={'Name':'Vibhuti','age':22,'course':'Computer Application','Subjects':['OS',"Networing",'java','Pyhon'],"City":'Bhopal'}
+print(student)
+print("\n")
+student.pop("City")
+print(student) 
+
+# print(student.get('Subjects'))
+# print("\n")
+    
+
+# print(student.keys())
+# print("\n")
+    
+
+# print(list(student.values()))
+# print("\n")
+    
+    
+# student['College']='UIT-RGPV'
+# print(student)
+# print("\n")
+    
+# student.update({'College':'CIST'})
+# print(student)
+# print("\n")
+    
+# for x in student:
+#     print(student[x])
+# print("\n")
+# for x in student.keys():
+#     print(x)
+    
+# print("\n")
+
+# for y in student.values():
+#     print(y)
+# print("\n")
+    
+# for x,y in student.items():
+#     print([x,y])
+    
+y=student
+print(y)
+
+del y
+print()
