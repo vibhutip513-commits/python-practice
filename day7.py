cbde22aa59a459f5db5d172a038c0eb6b4380c2e
Vibhuti Prajapati <vibhutip513@gmail.com>
2026-07-15 17:48:07 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,48 @@
+# #Sum and average of list
+# list=[1,2,3,4,5,6]
+# sum=0
+# for i in list:
+#     sum+=i
+
+# avg=sum/i
+# print("Sum=",sum)
+# print("Average=",avg)
+
+# list=[2,45,6,78,5.4,5]
+# print(max(list))
+
+# list=[2,45,6,78,5.4,5]
+# largest=list[0]
+# index=0
+# for i in range(len(list)):
+#     if list[i]>largest:
+#         largest=list[i]
+#         index=i
+# print(largest,index)
+
+
+# list=[2,45,6,78,5.4,5]
+# largest=list[0]
+# second_largest=list[0]
+# index=0
+# for i in range(len(list)):
+#     if list[i]>largest:
+#         largest=list[i]
+#         index=i
+    
+#     if second_largest>i
+# print(largest,index)
+
+
+
+# a=[12,12,34,4,54,5,6567,76,8879,54]
+# for i in range(len(a)-1):
+#     if a[i]<a[i+1]:
+#         continue
+#     else:
+#         print("Your list is not sorted")
+#         break
+# else:
+#     print("YOur list is sorted")
+
+
